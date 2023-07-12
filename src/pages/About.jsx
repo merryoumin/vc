d66dc2a81d7db32f886459970ce3a34bbebfb5c1
youmin <Merryoumin@gmail.com>
2023-07-13 03:52:51 +0900

    about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-
+import { FaGithub } from "react-icons/fa";
 function About({ account }) {
   return (
     <div>
-      <div className="flex justify-center items-end ">
+      <div className="flex justify-center  mb-40  items-end ">
         <div>
           <img
             className=" h-44 w-52 border-8 bg-neutral-700  rounded-full"
@@ -27,8 +27,56 @@ function About({ account }) {
         올라갑니다. 더 가볍고 안전한 서비스를 이용 할 수 있습니다. 모든 투표는
         공개되어 추적이 가능합니다.
       </div>
-      <div className="m-6 flex text-gray-200  mt-20 text-4xl text-left mr-4   items-end  justify-end  ">
+      <div className="m-6 flex text-gray-200  mt-18 text-4xl text-left mr-4   items-end  justify-end  ">
         It is hard and pure like a jewel and is oriented toward a voting system.
+      </div>
+      <div
+        style={{ fontFamily: "dia2" }}
+        className="mt-48 text-teal-200  text-4xl  text-center"
+      >
+        No pressure, no diamonds.
+      </div>
+      <div className="text-sx text-gray-300  text-right mr-12">
+        Thomas Carlyle.
+      </div>
+      <div
+        className="flex mt-10 justify-around text-slate-600"
+        style={{ fontFamily: "text" }}
+      >
+        <div className="ml-8  pb-32 ">
+          <a
+            href="https://github.com/merryoumin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="  h-36 w-36 bg-neutral-700  rounded-3xl"
+              src={process.env.PUBLIC_URL + "images/youmin.jpg"}
+              alt="aboutPic"
+            />
+          </a>
+          <div className="flex  justify-center items-center">
+            <FaGithub />
+            <div className=" ml-2 text-center">하유민</div>
+          </div>
+        </div>
+        <div className="mr-8 h-45">
+          <a
+            href="https://github.com/merryoumin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className=" h-36  w-36  bg-neutral-700  rounded-3xl"
+              src={process.env.PUBLIC_URL + "images/jeayoon.jpeg"}
+              alt="aboutPic"
+            />
+          </a>
+          <div className="flex justify-center items-center">
+            <FaGithub />
+            <div className="ml-2 text-center">황재윤</div>
+          </div>
+        </div>
       </div>
     </div>
   );
