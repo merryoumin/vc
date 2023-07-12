acab85bdb1e02dff9574e06c5feccbf5fdc3b480
youmin <Merryoumin@gmail.com>
2023-07-13 02:14:50 +0900

    contant

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,28 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import emailjs from "@emailjs/browser";
 function Contact() {
   const [account, setAccount] = useState();
+  const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_4jfuqdg",
+        "template_3q62ptk",
+        form.current,
+        "laKt2xfRmXUyY9YOu"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   useEffect(() => {
     if (!account) {
       setAccount(localStorage.getItem("address"));
@@ -10,9 +30,40 @@ function Contact() {
   }, []);
 
   return (
-    <div className="flex">
+    <div className=" justify-center items-center ">
+      <div className="contant">
+        <div className="text text-end mr-8 text-4xl text-gray-300">Contact</div>
+        <div className="text text-end mr-8 text-4xl  text-gray-300">Us</div>
+        <form ref={form} onSubmit={sendEmail}>
+          <div>
+            <div>
+              <label className="text-gray-600">Name</label>
+            </div>
+            <input type="text" name="user_name" />
+          </div>
+          <div>
+            <div>
+              <label className="text-gray-600">Email</label>
+            </div>
+            <input type="email" name="user_email" />
+          </div>
+          <div>
+            <div>
+              <label className="text-gray-600">Message</label>
+            </div>
+            <textarea name="message" />
+          </div>
+          <div className="mt-4">
+            <input
+              className="text-center btmSum h-14"
+              type="submit"
+              value="Send"
+            />
+          </div>
+        </form>
+      </div>
       <img
-        className="h-screen justify-center items-center  w-screen Contact"
+        className="h-screen contactPic justify-center items-center  w-screen Contact"
         src={process.env.PUBLIC_URL + "images/contact_f.gif"}
         alt="aboutPic"
       />
